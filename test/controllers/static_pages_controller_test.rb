58d5c694a2a2f144580b218597ada9e735868b9b
AngieChilds <pirateprincess_avh@yahoo.com>
2018-04-03 13:10:44 +0200

    finished user

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "The Irish Rock Sample App"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "The Irish Rock Sample App"
+  end
+  
+
+end
